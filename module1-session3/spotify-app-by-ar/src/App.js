7338a00d9838fa8ca450c429a74fa4b6c1c26334
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-12 14:31:27 +0700

    test env file

--- module1-session3/spotify-app-by-ar/src/App.js
+++ module1-session3/spotify-app-by-ar/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{process.env.REACT_APP_TITLE} - by Aulia Rosyida</h1>
+        <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
+
+        {process.env.NODE_ENV === 'development'
+          ? process.env.REACT_APP_DEV_MODE
+          : process.env.REACT_APP_PRO_MODE}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +21,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, coba munculin id:
+          {process.env.REACT_APP_SPOTIFY_KEY}
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
