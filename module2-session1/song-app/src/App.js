89971e7ec31701bf3d17677817636cfd0a03889b
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-14 16:38:22 +0700

    new project

--- module2-session1/song-app/src/App.js
+++ module2-session1/song-app/src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{process.env.REACT_APP_TITLE} - by Aulia Rosyida</h1>
-        <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
-
-        {process.env.NODE_ENV === 'development'
-          ? process.env.REACT_APP_DEV_MODE
-          : process.env.REACT_APP_PRO_MODE}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,12 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, coba munculin id:
-          {process.env.REACT_APP_SPOTIFY_KEY}
+          Learn React
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
