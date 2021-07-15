20a80e733e9ff17d0bea1b8a14ff09caf1b54935
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-16 00:00:25 +0700

    use props to put data from main page into card

--- module2-session1/song-app/src/App.js
+++ module2-session1/song-app/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg'
 import './App.css'
-import Card from './components/Card'
+import Card from './components/card/index'
+import songJson from '../api/song'
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card
+        albumName={songJson.album.name}
+        imgUrl={songJson.album.images[0].url}
+        artistName={songJson.album.artists[0].name}
+        albumName={songJson.album.name}
+      />
     </div>
   )
 }
