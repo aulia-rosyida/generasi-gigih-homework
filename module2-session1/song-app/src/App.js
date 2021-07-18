689c94659bcd04a987c670a01da872a6881f5c46
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-19 00:13:19 +0700

    loop data and create track components for each track list + beautify front end

--- module2-session1/song-app/src/App.js
+++ module2-session1/song-app/src/App.js
@@ -1,15 +1,26 @@
 import './App.css'
 import Card from './components/card/index'
-import songJson from './api/song'
+import tracks from './api/tracks'
 
 function App() {
   return (
     <div className="App">
-      <Card
-        albumName={songJson.album.name}
-        imageUrl={songJson.album.images[1].url}
-        artistName={songJson.album.artists[0].name}
-      />
+       {
+          tracks.map( track => {
+            return track && 
+            <div key={track?.id}>{
+              track &&
+              <div>
+                <Card
+                  albumName={track.album.name}
+                  imageUrl={track.album.images[1].url}
+                  artistName={track.album.artists[0].name}
+                />
+              </div>
+              }
+            </div>
+          })
+        }
     </div>
   )
 }
--- module2-session1/song-app/src/components/image/index.js
+++ module2-session1/song-app/src/components/image/index.js
@@ -1,5 +1,5 @@
 function Image(props) {
-    return <img src={props.url} alt="gif"  height="250" width="250"  />
+    return <img src={props.url} alt="gif"  height="50%" width="50%"  />
   }
   
   export default Image
