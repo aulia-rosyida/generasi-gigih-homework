a6faea1891558e2d9d4b9bcb3cf68cb7268edbe5
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-21 19:22:56 +0700

    change from class to functional

--- module2-session1/song-app/src/pages/Search.js
+++ module2-session1/song-app/src/pages/Search.js
@@ -1,72 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Search.css'
 import Image from '../components/image/index.js'
 
-function fetchAPI(param, callback) {
-  // param is a highlighted word from the user before it clicked the button
-  return fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=5mioeMFTkhOA8A2JSAbf6RmYwPHN9eME&q=${param}&limit=10`,
-  ).then(callback)
-}
-class Search extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { inputSearch: '', songs: null }
+const Search = () => {
+  const [gifs, setGifs] = useState([])
+  const [inputGif, setInputGif] = useState('input here')
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+  useEffect(() => {
+    getGif()
+  }, [])
 
-  handleChange(event) {
-    this.setState({ inputSearch: event.target.value })
-  }
-
-  handleSubmit(event) {
-    event.preventDefault()
+  const handleSubmit = (e) => {
+    setInputGif(e.target.value)
     console.log('masuk handle submit')
-    alert('A name was submitted: ' + this.state.inputSearch)
-    fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=5mioeMFTkhOA8A2JSAbf6RmYwPHN9eME&q=${this.state.inputSearch}&limit=10`,
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ songs: data })
-        console.log(this.state.songs)
-      })
+    alert('A name was submitted: ' + e.target.value)
   }
 
-  render() {
-    return (
-      <div className="search-form">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              value={this.state.inputSearch}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        {this.state.songs?.map((song) => {
-          return (
-            song && (
-              <div key={song?.id}>
-                {song && (
-                  <div>
-                    {/* <p>The rating is : {song.rating} </p> */}
-                    <p>The title is : {song.title}</p>
-                    {/* <Image url={song.images[0].url} /> */}
-                  </div>
-                )}
-              </div>
-            )
-          )
-        })}
-      </div>
-    )
+  const getGif = async () => {
+    const gifs = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=5mioeMFTkhOA8A2JSAbf6RmYwPHN9eME&q=${inputGif}&limit=10`,
+    ).then((response) => response.json())
+    setGifs(gifs)
+    console.log(gifs)
   }
+
+  return (
+    <div className="search-form">
+      {/* <form onSubmit={handleSubmit}> */}
+      <label>
+        Name:
+        <input
+          type="text"
+          value={inputGif}
+          onChange={(e) => setInputGif(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" onClick={getGif} />
+      {/* </form> */}
+      {gifs.data?.map((gif) => {
+        return (
+          gif && (
+            <div key={gif?.id}>
+              {gif && (
+                <div>
+                  <p>The rating is : {gif.rating} </p>
+                  <p>The title is : {gif.title}</p>
+                  <Image url={gif.images.original.url} />
+                </div>
+              )}
+            </div>
+          )
+        )
+      })}
+    </div>
+  )
 }
 
 export default Search
