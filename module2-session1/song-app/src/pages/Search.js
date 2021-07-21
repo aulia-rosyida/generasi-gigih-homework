feef3a82551250c19420f966ce6137cdabc1aa09
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-21 19:27:13 +0700

    show the 1st 12 result

--- module2-session1/song-app/src/pages/Search.js
+++ module2-session1/song-app/src/pages/Search.js
@@ -18,7 +18,7 @@ const Search = () => {
 
   const getGif = async () => {
     const gifs = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=5mioeMFTkhOA8A2JSAbf6RmYwPHN9eME&q=${inputGif}&limit=10`,
+      `https://api.giphy.com/v1/gifs/search?api_key=5mioeMFTkhOA8A2JSAbf6RmYwPHN9eME&q=${inputGif}&limit=12`,
     ).then((response) => response.json())
     setGifs(gifs)
     console.log(gifs)
