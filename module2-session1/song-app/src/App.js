e21179c2c4a7d12ccc167b450294e1f1325d1008
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-07-28 20:03:40 +0700

    use api, create trending page, reuse Gif component (done)

--- module2-session1/song-app/src/App.js
+++ module2-session1/song-app/src/App.js
@@ -1,10 +1,30 @@
 import './App.css'
 import Search from './pages/Search'
+import Trending from './pages/trending'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/trending">Trending</Link>
+            </li>
+          </ul>
+
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route path="/trending">
+            <Trending />
+          </Route>
+        </div>
+      </Router>
     </div>
   )
 }
